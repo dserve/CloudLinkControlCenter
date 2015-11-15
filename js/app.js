0d01e54ea5d1ab9728cfa7b6cb12506c2a2f6aa0
dserve <serve@fs.tum.de>
2015-11-15 14:55:16 +0100

    added some buttons

--- js/app.js
+++ js/app.js
@@ -11,6 +11,17 @@ function init(){
 	var moment = require('moment');
 	
 	global.$(global.window.document).ready(function(){
+			$("body").on('click', "#getTime", function(e) {
+				e.preventDefault();
+				console.log(global.$('#updatedSinceDate').val());
+				console.log($('#createdSinceTime').val());
+				rest.getOrdersFiltered('done', $('#createdSinceTime').val().toString(), $('#updatedSinceDate').val().toString(), "['Cocktails']")
+					.then(function(ret){
+						console.log(ret);
+					});
+
+			});
+		
 			rest.checkConnection()
 				.then(function(ret){
 					console.log(ret.status);
@@ -20,6 +31,8 @@ function init(){
 				.then(function(ret){
 					console.log(ret);
 				});
+			
+
 				
 			console.log(moment(new Date(2015,08,09,10,22)).format());
 			
